4b0b012b70836e085af9df0321a49a73a9b857db
Strati <m1r0x@live.de>
2018-01-27 08:44:48 +0100

    first build

--- CSharpSingleton/Program.cs
+++ CSharpSingleton/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace CSharpSingleton
 {
+    public class Singleton
+{
+    private static Singleton instance;
+
+    public int MyVal;
+
+    //private constructor
+
+    private Singleton() { }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
     class Program
     {
         static void Main(string[] args)
         {
+            Singleton.Instance.MyVal = 123;
+            Console.WriteLine(Singleton.Instance.MyVal);
+            Console.ReadKey();
         }
     }
 }
